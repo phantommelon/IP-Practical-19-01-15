828f6db7d763e123916243b1d24dd314b2f6dfe2
Alistair Madden <phantommelon@gmail.com>
2015-01-21 20:37:00 +0000

    Added relevent project files to netbeans.

---
+++ Company_Project/src/WholeItem.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class WholeItem here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WholeItem extends Item
+{
+
+    /**
+     * Constructor for objects of class WholeItem
+     */
+    public WholeItem(int id, String name, String description,
+    double vatPercent, double pricePerUnit)
+    {
+        super(id, name, description, vatPercent, pricePerUnit);
+    }
+
+}
